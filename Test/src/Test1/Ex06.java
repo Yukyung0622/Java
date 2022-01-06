2b1dd87a8cb1b732c83cb06c39ec8a1cf2de996f
unknown <kkomang0622@gmail.com>
2022-01-06 13:23:36 +0900

    2022/01/06 JAVA Test

---
+++ Test/src/Test1/Ex06.java
@@ -0,0 +1,13 @@
+package Test1;
+
+public class Ex06 {
+	
+	public static void main(String[] args) {
+		int x = 12;
+		int y = 21;
+		
+		String result = x>y?"x°¡ ´õ Å®´Ï´Ù." : "y°¡ ´õ Å®´Ï´Ù.";
+		System.out.println(result);
+	}
+
+}
---
+++ Test/src/Test2/Ex01.java
@@ -0,0 +1,21 @@
+package Test2;
+
+public class Ex01 {
+	
+	public static void main(String[] args) {
+		
+		char str [] = {'I',' ','L','O','V','E',' ','Y','O','U'};
+		int row, col;
+		
+		for(row = 1; row<10; row++) {
+			for(col =0; col <= row; col++) {
+			
+				System.out.print(str[col]);	
+				
+				
+			}
+			System.out.print("\n");
+		}
+	}
+
+}
